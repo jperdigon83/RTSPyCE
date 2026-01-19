44527e72b697584b66f3f247d44703777b3f3810
jperdigon <jeremy.perdigon@oca.eu>
2026-01-19 13:29:45 +0100

    adding logos

---
+++ tests/hydogen_disc.py
@@ -0,0 +1,14 @@
+import sys
+sys.append("/home/jperdigon/projects/rtspyce/src")
+from envelope import Envelope
+
+
+class HydrogenDisc(Envelope):
+
+    def __init__(self, params):
+
+        # Parameters
+
+
+        
+        
